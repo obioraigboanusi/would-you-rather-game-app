98ed51c91e511766564be9ad117745cc075533ef
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-10-02 16:13:24 +0100

    chore: add react-router-dom package

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './components/App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
