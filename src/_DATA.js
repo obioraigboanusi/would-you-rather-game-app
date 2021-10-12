31be5254f753bb3594c0ffb1926a284c1c835f83
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-10-12 17:51:00 +0100

    feat: add avata url

--- src/_DATA.js
+++ src/_DATA.js
@@ -1,8 +1,9 @@
+import avatar from "./assets/jenny.jpg";
 let users = {
   sarahedo: {
     id: "sarahedo",
     name: "Sarah Edo",
-    avatarURL: "./assets/jenny.jpg",
+    avatarURL: avatar,
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -14,7 +15,7 @@ let users = {
   tylermcginnis: {
     id: "tylermcginnis",
     name: "Tyler McGinnis",
-    avatarURL: "./assets/jenny.jpg",
+    avatarURL: avatar,
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo",
@@ -24,7 +25,7 @@ let users = {
   johndoe: {
     id: "johndoe",
     name: "John Doe",
-    avatarURL: "./assets/jenny.jpg",
+    avatarURL: avatar,
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
