b658447b90d23ff888ed4a26b11cd056da7f8775
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-10-17 18:45:46 +0100

    feat: update avatar url

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,9 +1,11 @@
-import avatar from "../assets/jenny.jpg";
+import jenny from "../assets/jenny.jpg";
+import elliot from "../assets/elliot.jpg";
+import molly from "../assets/molly.jpg";
 let users = {
   sarahedo: {
     id: "sarahedo",
     name: "Sarah Edo",
-    avatarURL: avatar,
+    avatarURL: jenny,
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -15,7 +17,7 @@ let users = {
   tylermcginnis: {
     id: "tylermcginnis",
     name: "Tyler McGinnis",
-    avatarURL: avatar,
+    avatarURL: molly,
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo",
@@ -25,7 +27,7 @@ let users = {
   johndoe: {
     id: "johndoe",
     name: "John Doe",
-    avatarURL: avatar,
+    avatarURL: elliot,
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
