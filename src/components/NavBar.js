3d9084bfe99407388207412607154b0ec712d246
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-10-17 07:09:57 +0100

    feat: add navlinks active state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import { connect } from "react-redux";
 import UserTag from "./UserTag";
-
+const navLinks = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/add",
+    text: "Add",
+  },
+  {
+    url: "/leaderboard",
+    text: "LeaderBoard",
+  },
+];
 function NavBar({ user }) {
+  const history = useHistory();
   return (
     <div className="nav-cont">
       <div className="nav">
         <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/add">Add</Link>
-          <Link to="/leaderboard">LeaderBoard</Link>
+          {navLinks.map((link) => (
+            <Link
+              to={link.url}
+              className={history.location.pathname === link.url ? "active" : ""}
+            >
+              {link.text}
+            </Link>
+          ))}
         </div>
         {!!user && <UserTag user={user} />}
       </div>
