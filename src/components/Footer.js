fd6adb850c2e9c46710ddbfd103ba2ae8bf73095
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-10-17 17:11:50 +0100

    feat: create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <small>
+          <p>Copyright &copy; 2021 | All rights reserved</p>
+          <p>
+            This app was developed with{" "}
+            <i className="ui heart icon red large"></i>by{" "}
+            <a href="https://linkedin.com/in/obioraigboanusi" target="_blank">
+              Obiora Igboanusi
+            </a>
+          </p>
+        </small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
